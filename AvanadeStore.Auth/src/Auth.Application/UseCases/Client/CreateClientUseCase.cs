d790fce58918ac5b2a2f404906f218b217f57d0d
b01tech <bruno01.tech@gmail.com>
2025-09-05 12:08:35 -0300

    feat(client): add create client validator

--- AvanadeStore.Auth/src/Auth.Application/UseCases/Client/CreateClientUseCase.cs
+++ AvanadeStore.Auth/src/Auth.Application/UseCases/Client/CreateClientUseCase.cs
@@ -1,12 +1,25 @@
 using Auth.Application.DTOs.Requests;
 using Auth.Application.DTOs.Responses;
+using Auth.Exception.CustomExceptions;
 
 namespace Auth.Application.UseCases.Client;
 internal class CreateClientUseCase : ICreateClientUseCase
 {
-    public Task<ResponseCreateUserDTO> ExecuteAsync(RequestCreateClientDTO request)
+    public async Task<ResponseCreateUserDTO> ExecuteAsync(RequestCreateClientDTO request)
     {
+        await ValidateAsync(request);
         var result = new ResponseCreateUserDTO(Guid.CreateVersion7(), request.Name, DateTime.UtcNow);
-        return Task.FromResult(result);
+        return await Task.FromResult(result);
+    }
+
+    private static async Task ValidateAsync(RequestCreateClientDTO request)
+    {
+        var validator = new CreateClientValidator();
+        var result = await validator.ValidateAsync(request);
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
+            throw new OnValidationException(errorMessages);
+        }          
     }
 }
--- AvanadeStore.Auth/src/Auth.Application/UseCases/Client/CreateClientValidator.cs
+++ AvanadeStore.Auth/src/Auth.Application/UseCases/Client/CreateClientValidator.cs
@@ -1,8 +1,18 @@
+using Auth.Application.DTOs.Requests;
+using Auth.Exception.ErrorMessages;
+using FluentValidation;
+
 namespace Auth.Application.UseCases.Client;
-public class CreateClientValidator
+public class CreateClientValidator : AbstractValidator<RequestCreateClientDTO>
 {
     public CreateClientValidator()
     {
-        
+        RuleFor(u => u.Name).NotEmpty().WithMessage(ResourceErrorMessages.NAME_EMPTY);
+        RuleFor(u => u.Email).NotEmpty().WithMessage(ResourceErrorMessages.EMAIL_EMPTY);
+        RuleFor(u => u.Password.Length).GreaterThanOrEqualTo(6).WithMessage(ResourceErrorMessages.PASSWORD_INVALID);
+        When(u => !string.IsNullOrEmpty(u.Email), () =>
+        {
+            RuleFor(u => u.Email).EmailAddress().WithMessage(ResourceErrorMessages.EMAIL_INVALID);
+        });
     }
 }
