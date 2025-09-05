3509d496cbbb7cc00ee3175831bd9a8826197500
b01tech <bruno01.tech@gmail.com>
2025-09-05 14:49:54 -0300

    feat: add infrastructure to di container

--- AvanadeStore.Auth/src/Auth.API/Program.cs
+++ AvanadeStore.Auth/src/Auth.API/Program.cs
@@ -1,13 +1,15 @@
 using Auth.API.Extensions;
 using Auth.API.Middlewares;
 using Auth.Application.Extensions;
+using Auth.Infra.Extensions;
 
 DotNetEnv.Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddApiDocumentation()
     .AddUseCases()
-    .AddServices();
+    .AddServices()
+    .AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 app.MapApiDocumentation();
