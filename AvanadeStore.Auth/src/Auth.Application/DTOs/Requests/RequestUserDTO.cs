be7d0b2404cec37fc1de90a141b743f8b925051f
b01tech <bruno01.tech@gmail.com>
2025-09-05 16:53:00 -0300

    feat: add method to hash password

--- AvanadeStore.Auth/src/Auth.Application/DTOs/Requests/RequestUserDTO.cs
+++ AvanadeStore.Auth/src/Auth.Application/DTOs/Requests/RequestUserDTO.cs
@@ -1,5 +1,8 @@
 using Auth.Domain.Enums;
 
 namespace Auth.Application.DTOs.Requests;
-public record RequestCreateClientDTO(string Name, string Email, string Cpf, string Password);
+public record RequestCreateClientDTO(string Name, string Email, string Cpf, string Password)
+{
+    public RequestCreateClientDTO WithHashedPassword(string hashedPassword) => this with { Password = hashedPassword };    
+}
 public record RequestCreateEmployeeDTO(string Name, string Email, string Password, UserRole Role);
