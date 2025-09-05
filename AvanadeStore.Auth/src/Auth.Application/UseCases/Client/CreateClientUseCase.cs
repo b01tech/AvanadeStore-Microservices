1021ff625acc4b820bd606f280871cbc85a2f300
b01tech <bruno01.tech@gmail.com>
2025-09-05 16:55:41 -0300

    feat(client): add mapping service and repository integration for client creation

--- AvanadeStore.Auth/src/Auth.Application/UseCases/Client/CreateClientUseCase.cs
+++ AvanadeStore.Auth/src/Auth.Application/UseCases/Client/CreateClientUseCase.cs
@@ -1,24 +1,37 @@
 using Auth.Application.DTOs.Requests;
 using Auth.Application.DTOs.Responses;
 using Auth.Application.Services.Criptography;
+using Auth.Domain.Interfaces;
 using Auth.Exception.CustomExceptions;
+using MapsterMapper;
 
 namespace Auth.Application.UseCases.Client;
 internal class CreateClientUseCase : ICreateClientUseCase
 {
     private readonly IEncrypter _encrypter;
+    private readonly IClientRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IMapper _mapper;
 
-    public CreateClientUseCase(IEncrypter encrypter)
+    public CreateClientUseCase(IEncrypter encrypter, IClientRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
     {
         _encrypter = encrypter;
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+        _mapper = mapper;
     }
 
     public async Task<ResponseCreateUserDTO> ExecuteAsync(RequestCreateClientDTO request)
     {
         await ValidateAsync(request);
         var hashedPassword = _encrypter.Encrypt(request.Password);
-        var result = new ResponseCreateUserDTO(Guid.CreateVersion7(), request.Name, DateTime.UtcNow);
-        return await Task.FromResult(result);
+        var client = _mapper.Map<Domain.Entities.Client>(request.WithHashedPassword(hashedPassword));
+
+        await _repository.AddAsync(client);
+        await _unitOfWork.CommitAsync();
+
+        var response = _mapper.Map<ResponseCreateUserDTO>(client);
+        return await Task.FromResult(response);
     }
 
     private static async Task ValidateAsync(RequestCreateClientDTO request)
