411dfa513b98d8ab846d44613d0e4a4e670a5227
b01tech <bruno01.tech@gmail.com>
2025-09-06 08:18:13 -0300

    feat: add client login endpoints

--- AvanadeStore.Auth/src/Auth.API/Extensions/EndpointsExtension.cs
+++ AvanadeStore.Auth/src/Auth.API/Extensions/EndpointsExtension.cs
@@ -8,6 +8,7 @@ public static class EndpointsExtension
     public static WebApplication MapEndpoints(this WebApplication app)
     {
         MapClientEndpoints(app);
+        MapLoginEndpoints(app);
         return app;
     }
 
@@ -15,11 +16,22 @@ public static class EndpointsExtension
     {
         var group = app.MapGroup("/client").WithTags("Client").WithOpenApi();
 
-        app.MapPost("/", async (RequestCreateClientDTO request, ICreateClientUseCase useCase) =>
+        group.MapPost("/", async (RequestCreateClientDTO request, ICreateClientUseCase useCase) =>
         {
             var result = await useCase.ExecuteAsync(request);
             return Results.Ok(result);
         });
-
+    }
+    private static void MapLoginEndpoints(WebApplication app)
+    {
+        var group = app.MapGroup("/login").WithTags("Login").WithOpenApi();
+        group.MapPost("/cpf", () =>
+        {
+            return Results.Ok("login by cpf endpoint");
+        });
+        group.MapPost("/email", () =>
+        {
+            return Results.Ok("login by email endpoint");
+        });
     }
 }
