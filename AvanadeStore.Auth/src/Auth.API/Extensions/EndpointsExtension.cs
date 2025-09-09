4bb7404cda266444f3f8e4a83eee4eed41204739
b01tech <bruno01.tech@gmail.com>
2025-09-09 10:33:30 -0300

    refactor: change endpoints descriptions

--- AvanadeStore.Auth/src/Auth.API/Extensions/EndpointsExtension.cs
+++ AvanadeStore.Auth/src/Auth.API/Extensions/EndpointsExtension.cs
@@ -20,21 +20,24 @@ public static class EndpointsExtension
         group.MapPost("/", async (RequestCreateClientDTO request, ICreateClientUseCase useCase) =>
         {
             var result = await useCase.ExecuteAsync(request);
-            return Results.Ok(result);
-        });
+            return Results.Created(string.Empty, result);
+        }).WithDescription("Faz cadastro de clientes")
+        .Produces(StatusCodes.Status201Created).Produces(StatusCodes.Status400BadRequest);
     }
     private static void MapLoginEndpoints(WebApplication app)
     {
         var group = app.MapGroup("/login").WithTags("Login").WithOpenApi();
         group.MapPost("/cpf", async (RequestLoginByCpfDTO request, ILoginUseCase useCase) =>
         {
-            var result = await useCase.LoginByCpf(request); 
+            var result = await useCase.LoginByCpf(request);
             return Results.Ok(result);
-        });
+        }).WithDescription("Realiza login com CPF")
+        .Produces(StatusCodes.Status200OK).Produces(StatusCodes.Status400BadRequest).Produces(StatusCodes.Status401Unauthorized);
         group.MapPost("/email", async (RequestLoginByEmailDTO request, ILoginUseCase useCase) =>
         {
             var result = await useCase.LoginByEmail(request);
             return Results.Ok(result);
-        });
+        }).WithDescription("Realiza login com Email")
+        .Produces(StatusCodes.Status200OK).Produces(StatusCodes.Status400BadRequest).Produces(StatusCodes.Status401Unauthorized);
     }
 }
