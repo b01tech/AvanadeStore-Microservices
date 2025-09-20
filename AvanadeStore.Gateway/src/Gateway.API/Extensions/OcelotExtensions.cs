fc3a211992543dc728a8c97f44f2091f0ab0e2b6
b01tech <bruno01.tech@gmail.com>
2025-09-20 19:08:12 -0300

    refactor(gateway): consolidate ocelot configuration files

--- AvanadeStore.Gateway/src/Gateway.API/Extensions/OcelotExtensions.cs
+++ AvanadeStore.Gateway/src/Gateway.API/Extensions/OcelotExtensions.cs
@@ -2,12 +2,11 @@ namespace Gateway.API.Extensions;
 
 public static class OcelotExtensions
 {
-    public static ConfigurationManager AddOcelotConfigurations(this ConfigurationManager configuration)
+    public static ConfigurationManager AddOcelotConfigurations(
+        this ConfigurationManager configuration
+    )
     {
         configuration.AddJsonFile("Settings/ocelot.json", optional: false, reloadOnChange: true);
-        configuration.AddJsonFile("Settings/ocelot.auth.json", optional: false, reloadOnChange: true);
-        configuration.AddJsonFile("Settings/ocelot.sales.json", optional: false, reloadOnChange: true);
-        configuration.AddJsonFile("Settings/ocelot.inventory.json", optional: false, reloadOnChange: true);
         return configuration;
     }
 }
